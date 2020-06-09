3b2ca9418b368bbd5ccce78dbe0fc1845255d1d5
Adrian Carter <lee@MacBurnedChickenNugget.local>
2020-06-09 10:32:48 +0100

    added script to enumerate samba version from a machine thats not playing ball

---
+++ enumeration-scripts/smbversion.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#Author: lee carter
+#Description:
+# Requires root or enough permissions to use tcpdump
+if [ -z $1 ]; then echo "Usage: ./smbversion.sh RHOST INTERFACE {RPORT}" && exit; else rhost=$1; fi
+if [ ! -z $3 ]; then rport=$3; else rport=139; fi
+if [ ! -z $2 ]; then interface=$2; else interface=tun0; fi
+tcpdump -s0 -n -i $interface src $rhost and port $rport -A -c 7 2>/dev/null | grep -i "samba\|s.a.m" | tr -d '.' | grep -oP 'UnixSamba.*[0-9a-z]' | tr -d '\n' & echo -n "$rhost: " &
+echo "exit" | smbclient -L $rhost 1>/dev/null 2>/dev/null
+echo "" && sleep .1
